2f96be9877ce96e630c835820895f3856ed3f413
Alfredo Ficcadenti <aficcadenti@homeaway.com>
2020-02-21 21:07:25 +0000

    grouped battle history

--- battlesGetData.js
+++ battlesGetData.js
@@ -23,116 +23,104 @@ async function getBattleHistory(player = '', data = {}) {
     return battleHistory.battles;
 }
 
-users = [];
-player = 'a1492dc'
+const extractGeneralInfo = (x) => {
+    return {
+        created_date: x.created_date ? x.created_date : '',
+        match_type: x.match_type ? x.match_type : '',
+        mana_cap: x.mana_cap ? x.mana_cap : '',
+        ruleset: x.ruleset ? x.ruleset : '',
+        inactive: x.inactive ? x.inactive : ''
+    }
+}
 
-usersToGrab =  ['aaken','a1492dc','kenchung3','geels','kenchung1','nutus','berindon','funmeko','tomeofsplinters','kenchung7','kenchung6','raferti','pal.aaronli','craftsman252','minnow.helper','xavezar','vyulivsaz','kenchung5']
-usersToGrab.forEach(element => {
-    getBattleHistory(element)
-    .then(battles => battles.map(
-        x => {
-            const details = JSON.parse(x.details);
-            if (JSON.parse(x.details).type != 'Surrender') {
-                if (x.winner == x.player_1) {
-                    const monster1 = JSON.parse(x.details).team1.monsters[0];
-                    const monster2 = JSON.parse(x.details).team1.monsters[1];
-                    const monster3 = JSON.parse(x.details).team1.monsters[2];
-                    const monster4 = JSON.parse(x.details).team1.monsters[3];
-                    const monster5 = JSON.parse(x.details).team1.monsters[4];
-                    const monster6 = JSON.parse(x.details).team1.monsters[5];
-                    return {
-                        battle_queue_id: x.battle_queue_id_1,
-                        player_rating_initial: x.player_1_rating_initial,
-                        player_rating_final: x.player_1_rating_final,
-                        player_name: x.player_1,
-                        created_date: x.created_date,
-                        match_type: x.match_type,
-                        mana_cap: x.mana_cap,
-                        current_streak: x.current_streak,
-                        ruleset: x.ruleset,
-                        inactive: x.inactive,
-                        details_team: x.team1,
-                        details_prebattle: x.prebattle,
-                        summoner_id: JSON.parse(x.details).team1.summoner.card_detail_id,
-                        summoner_level: JSON.parse(x.details).team1.summoner.level,
-                        monster_1_id: monster1 ? monster1.card_detail_id : '',
-                        monster_1_level: monster1 ? monster1.level : '',
-                        monster_1_abilities: monster1 ? monster1.abilities : '',
-                        monster_2_id: monster2 ? monster2.card_detail_id : '',
-                        monster_2_level: monster2 ? monster2.level : '',
-                        monster_2_abilities: monster2 ? monster2.abilities : '',
-                        monster_3_id: monster3 ? monster3.card_detail_id : '',
-                        monster_3_level: monster3 ? monster3.level : '',
-                        monster_3_abilities: monster3 ? monster3.abilities : '',
-                        monster_4_id: monster4 ? monster4.card_detail_id : '',
-                        monster_4_level: monster4 ? monster4.level : '',
-                        monster_4_abilities: monster4 ? monster4.abilities : '',
-                        monster_5_id: monster5 ? monster5.card_detail_id : '',
-                        monster_5_level: monster5 ? monster5.level : '',
-                        monster_5_abilities: monster5 ? monster5.abilities : '',
-                        monster_6_id: monster6 ? monster6.card_detail_id : '',
-                        monster_6_level: monster6 ? monster6.level : '',
-                        monster_6_abilities: monster6 ? monster6.abilities : ''
-                    }
-                } else if (x.winner == x.player_2) {
-                    const monster1 = JSON.parse(x.details).team2.monsters[0];
-                    const monster2 = JSON.parse(x.details).team2.monsters[1];
-                    const monster3 = JSON.parse(x.details).team2.monsters[2];
-                    const monster4 = JSON.parse(x.details).team2.monsters[3];
-                    const monster5 = JSON.parse(x.details).team2.monsters[4];
-                    const monster6 = JSON.parse(x.details).team2.monsters[5];
-                    
-                    return {
-                        battle_queue_id: x.battle_queue_id_2,
-                        player_rating_initial: x.player_2_rating_initial,
-                        player_rating_final: x.player_2_rating_final,
-                        player_name: x.player_2,
-                        created_date: x.created_date,
-                        match_type: x.match_type,
-                        mana_cap: x.mana_cap,
-                        current_streak: x.current_streak,
-                        ruleset: x.ruleset,
-                        inactive: x.inactive,
-                        details_team2: details.team2,
-                        details_prebattle: details.prebattle,
-                        summoner_id: JSON.parse(x.details).team2.summoner.card_detail_id,
-                        summoner_level: JSON.parse(x.details).team2.summoner.level,
-                        monster_1_id: monster1 ? monster1.card_detail_id : '',
-                        monster_1_level: monster1 ? monster1.level : '',
-                        monster_1_abilities: monster1 ? monster1.abilities : '',
-                        monster_2_id: monster2 ? monster2.card_detail_id : '',
-                        monster_2_level: monster2 ? monster2.level : '',
-                        monster_2_abilities: monster2 ? monster2.abilities : '',
-                        monster_3_id: monster3 ? monster3.card_detail_id : '',
-                        monster_3_level: monster3 ? monster3.level : '',
-                        monster_3_abilities: monster3 ? monster3.abilities : '',
-                        monster_4_id: monster4 ? monster4.card_detail_id : '',
-                        monster_4_level: monster4 ? monster4.level : '',
-                        monster_4_abilities: monster4 ? monster4.abilities : '',
-                        monster_5_id: monster5 ? monster5.card_detail_id : '',
-                        monster_5_level: monster5 ? monster5.level : '',
-                        monster_5_abilities: monster5 ? monster5.abilities : '',
-                        monster_6_id: monster6 ? monster6.card_detail_id : '',
-                        monster_6_level: monster6 ? monster6.level : '',
-                        monster_6_abilities: monster6 ? monster6.abilities : ''
-                    }
-            }}
+const extractMonster = (team) => {
+    const monster1 = team.monsters[0];
+    const monster2 = team.monsters[1];
+    const monster3 = team.monsters[2];
+    const monster4 = team.monsters[3];
+    const monster5 = team.monsters[4];
+    const monster6 = team.monsters[5];
 
-        })
-    ).then(
-        x=> {
-                fs.writeFile(`data/${element}.json`, JSON.stringify(x), function(err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-                x.map(element => 
-                    {
-                        console.log('elem',element)
-                        element && element.player_name ? users.push(element.player_name) : null;
-                    }
-                );
-                console.log(users.filter(distinct))
-            }
-    )
-});
+    return {summoner_id: team.summoner.card_detail_id,
+    summoner_level: team.summoner.level,
+    monster_1_id: monster1 ? monster1.card_detail_id : '',
+    monster_1_level: monster1 ? monster1.level : '',
+    monster_1_abilities: monster1 ? monster1.abilities : '',
+    monster_2_id: monster2 ? monster2.card_detail_id : '',
+    monster_2_level: monster2 ? monster2.level : '',
+    monster_2_abilities: monster2 ? monster2.abilities : '',
+    monster_3_id: monster3 ? monster3.card_detail_id : '',
+    monster_3_level: monster3 ? monster3.level : '',
+    monster_3_abilities: monster3 ? monster3.abilities : '',
+    monster_4_id: monster4 ? monster4.card_detail_id : '',
+    monster_4_level: monster4 ? monster4.level : '',
+    monster_4_abilities: monster4 ? monster4.abilities : '',
+    monster_5_id: monster5 ? monster5.card_detail_id : '',
+    monster_5_level: monster5 ? monster5.level : '',
+    monster_5_abilities: monster5 ? monster5.abilities : '',
+    monster_6_id: monster6 ? monster6.card_detail_id : '',
+    monster_6_level: monster6 ? monster6.level : '',
+    monster_6_abilities: monster6 ? monster6.abilities : ''}
+}
+
+let users = [];
+let battlesList = [];
+//usersToGrab =  ['aaken','a1492dc','kenchung3','geels','kenchung1','nutus','berindon','funmeko','tomeofsplinters','kenchung7','kenchung6','raferti','pal.aaronli','craftsman252','minnow.helper','xavezar','vyulivsaz','kenchung5']
+usersToGrab =  ['aaken','a1492dc'];
+const battles = usersToGrab.map(user => 
+    getBattleHistory(user)
+        .then(battles => battles.map(
+            battle => {
+                const details = JSON.parse(battle.details);
+                if (details.type != 'Surrender') {
+                    if (battle.winner && battle.winner == battle.player_1) {
+                        const monstersDetails = extractMonster(details.team1)
+                        const info = extractGeneralInfo(battle)
+                        return {
+                            ...monstersDetails,
+                            ...info,
+                            battle_queue_id: battle.battle_queue_id_1,
+                            player_rating_initial: battle.player_1_rating_initial,
+                            player_rating_final: battle.player_1_rating_final,
+                            winner: battle.player_1,
+                            
+                        }
+                    } else if (battle.winner && battle.winner == battle.player_2) {
+                        const monstersDetails = extractMonster(details.team2)
+                        const info = extractGeneralInfo(battle)
+                        return {
+                            ...monstersDetails,
+                            ...info,
+                            battle_queue_id: battle.battle_queue_id_2,
+                            player_rating_initial: battle.player_2_rating_initial,
+                            player_rating_final: battle.player_2_rating_final,
+                            winner: battle.player_2,
+                        }
+                    }}
+
+            })
+        ).then(x => battlesList.push(x))
+)
+
+Promise.all(battles).then(() => {
+    console.log(battles)
+    fs.writeFile(`data/history.json`, JSON.stringify(battlesList), function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+  });
+
+
+// fs.writeFile(`data/history.json`, JSON.stringify(battlesList), function(err) {
+//                 if (err) {
+//                     console.log(err);
+//                 }
+//             });
+            // //print all the winners
+            // x.map(element => 
+            //     {
+            //         element && element.winner ? users.push(element.winner) : null;
+            //     }
+            // );
+            // console.log(users.filter(distinct))
