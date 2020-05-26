b8644dea7bc8c63cfc7a70f36e6a65286eef21e5
Alfredo <alfredoficcadenti@gmail.com>
2020-05-27 00:33:31 +0100

    code refactor

--- helper.js
+++ helper.js
@@ -1,9 +1,10 @@
 
 const cardsDetails = require("./data/cardsDetails.json");
+const card = require("./cards")
 
 // const teamIdsArray = [167, 192, 160, 161, 163, 196, '', 'fire'];
 
-cardColor = (id) => cardsDetails.find(o => o.id === id) ? cardsDetails.find(o => o.id === id).color : '';
+//cardColor = (id) => cardsDetails.find(o => o.id === id) ? cardsDetails.find(o => o.id === id).color : '';
 
 const validDecks = ['Red', 'Blue', 'White', 'Black', 'Green']
 const colorToDeck = { 'Red': 'Fire', 'Blue': 'Water', 'White': 'Life', 'Black': 'Death', 'Green': 'Earth' }
@@ -15,30 +16,11 @@ const colorToDeck = { 'Red': 'Fire', 'Blue': 'Water', 'White': 'Life', 'Black': 
 //     }
 // })
 
-const deckValidColor = (accumulator, currentValue) => validDecks.includes(cardColor(currentValue)) ? colorToDeck[cardColor(currentValue)] : accumulator;
+const deckValidColor = (accumulator, currentValue) => validDecks.includes(card.color(currentValue)) ? colorToDeck[card.color(currentValue)] : accumulator;
 
 //console.log(teamIdsArray.reduce(deckValidColor, ''));
 
-const teamSplinterToPlay = (teamIdsArray) => teamIdsArray.reduce(deckValidColor, '')
+const teamActualSplinterToPlay = (teamIdsArray) => teamIdsArray.reduce(deckValidColor, '')
 
-const mostWinningSummonerTank = (possibleTeamsList) => {
-    mostWinningDeck = { fire: 0, death: 0, earth: 0, water: 0, life: 0 }
-    const mostWinningSummoner = {};
-    const mostWinningTank = {};
-    possibleTeamsList.forEach(x => {
-        const summoner = x[0];
-        mostWinningSummoner[summoner] = mostWinningSummoner[summoner] ? mostWinningSummoner[summoner] + 1 : 1;
-    })
-    const bestSummoner = Object.keys(mostWinningSummoner).reduce((a, b) => mostWinningSummoner[a] > mostWinningSummoner[b] ? a : b);
-    possibleTeamsList.filter(team => team[0] == bestSummoner).forEach(team => {
-        const tank = team[1];
-        console.log(mostWinningTank)
-        mostWinningTank[tank] = mostWinningTank[tank] ? mostWinningTank[tank] + 1 : 1;
-    })
-    //const bestTank = mostWinningTank && Object.keys(mostWinningTank).reduce((a, b) => mostWinningTank[a] > mostWinningTank[b] ? a : b);
-    // return { bestSummoner: bestSummoner, summonerWins: mostWinningSummoner[bestSummoner], tankWins: mostWinningTank[bestTank], bestTank: bestTank }
-    return { bestSummoner: bestSummoner, summonerWins: mostWinningSummoner[bestSummoner] }
-}
 
-module.exports.teamSplinterToPlay = teamSplinterToPlay;
-module.exports.mostWinningSummonerTank = mostWinningSummonerTank;
+module.exports.teamActualSplinterToPlay = teamActualSplinterToPlay;
--- index.js
+++ index.js
@@ -35,108 +35,98 @@ async function startBotPlayMatch() {
     await page.waitFor(30000);
 
     await page.waitForSelector('.btn--create-team', { timeout: 90000 })
-        //then read rules and details 
-        .then(async () => {
-            let [mana, rules, splinters] = await Promise.all([
-                splinterlandsPage.checkMatchMana(page).then((mana) => mana).catch(() => 'no mana'),
-                splinterlandsPage.checkMatchRules(page).then((rulesArray) => rulesArray).catch(() => 'no rules'),
-                splinterlandsPage.checkMatchActiveSplinters(page).then((splinters) => splinters).catch(() => 'no splinters')
-            ]);
-            return { 
-                mana: mana, 
-                rules: rules, 
-                splinters: splinters, 
-                myCards: myCards 
-            }
-        })
-        .then((matchDetails) => [ask.possibleTeams(matchDetails), matchDetails])
-        .then(([possibleTeams, matchDetails]) => {
-            if (possibleTeams) {
-                page.click('.btn--create-team')[0];
-                return [possibleTeams, matchDetails]
-            }
-            page.click('.btn--surrender')[0];
-            console.log('NO TEAMS');
-            throw new Error('no team available to be played');
-        })
-        .then(async ([possibleTeams, matchDetails]) => {
-            possibleTeams && possibleTeams.length ? 
-                console.log('Possible Teams: ', possibleTeams.length) : 
-                console.log('NO TEAMS');
-            
-            const bestCombination = battles.mostWinningSummonerTank(possibleTeams)
-            console.log('BEST SUMMONER and TANK', bestCombination)
-            if (bestCombination.summonerWins > 1) {
-                const bestTeam = await possibleTeams.find(x => x[0] == bestCombination.bestSummoner)
-                console.log('BEST TEAM', bestTeam)
-                if (matchDetails.splinters.includes(helper.teamSplinterToPlay(bestTeam).toLowerCase())) {
-                    console.log('PLAY BEST SUMMONER and TANK: ', helper.teamSplinterToPlay(bestTeam), bestTeam)
-                    const summoner = card.makeCardId(bestTeam[0].toString());
-                    return [summoner, bestTeam];
-                }
+    let [mana, rules, splinters] = await Promise.all([
+        splinterlandsPage.checkMatchMana(page).then((mana) => mana).catch(() => 'no mana'),
+        splinterlandsPage.checkMatchRules(page).then((rulesArray) => rulesArray).catch(() => 'no rules'),
+        splinterlandsPage.checkMatchActiveSplinters(page).then((splinters) => splinters).catch(() => 'no splinters')
+    ]);
 
-            }
+    const matchDetails = { 
+        mana: mana, 
+        rules: rules, 
+        splinters: splinters, 
+        myCards: myCards 
+    }
 
-            //TO UNCOMMENT FOR QUEST after choose the color
-            if (matchDetails.splinters.includes('fire') && possibleTeams.find(x => x[7] === 'fire')) {
-                const fireTeam = possibleTeams.find(x => x[7] === 'fire')
-                try {
-                    if (matchDetails.splinters.includes(helper.teamSplinterToPlay(fireTeam).toLowerCase())) {
-                        console.log('PLAY fire: ', helper.teamSplinterToPlay(fireTeam), fireTeam)
-                        const summoner = card.makeCardId(fireTeam[0].toString());
-                        return [summoner, fireTeam];
-                    }
-                    //console.log('fire but deck not active')
+    const possibleTeams = await ask.possibleTeams(matchDetails);
 
-                } catch (e) {
-                    console.log('fire DECK ERROR: ', e)
-                }
-            }
+    if(possibleTeams && possibleTeams.length) {
+        console.log('Possible Teams: ', possibleTeams.length);
+    } else {
+        //page.click('.btn--surrender')[0]; //commented to prevent alert dialog
+        throw new Error('NO TEAMS available to be played');
+    }
 
+    //TEAM SELECTION
+    let teamToPlay = [];
+    let i = 0;
+    for (i = 0; i < possibleTeams.length -1; i++) {
+        if (matchDetails.splinters.includes(possibleTeams[i][7]) && helper.teamSplinterToPlay(possibleTeams[i] !== '')) {
+            console.log('SELECTED: ', possibleTeams[i]);
+            const summoner = card.makeCardId(possibleTeams[i][0].toString());
+            teamToPlay = [summoner, possibleTeams[i]];
+        }
+        console.log('DISCARDED: ', possibleTeams[i])
+    }
+            // const bestCombination = battles.mostWinningSummonerTank(possibleTeams)
+            // console.log('BEST SUMMONER and TANK', bestCombination)
+            // if (bestCombination.summonerWins > 1) {
+            //     const bestTeam = await possibleTeams.find(x => x[0] == bestCombination.bestSummoner)
+            //     console.log('BEST TEAM', bestTeam)
+            //     if (matchDetails.splinters.includes(helper.teamSplinterToPlay(bestTeam).toLowerCase())) {
+            //         console.log('PLAY BEST SUMMONER and TANK: ', helper.teamSplinterToPlay(bestTeam), bestTeam)
+            //         const summoner = card.makeCardId(bestTeam[0].toString());
+            //         return [summoner, bestTeam];
+            //     }
 
-            let i = 0;
-            while (i <= possibleTeams.length - 1) {
-                if (matchDetails.splinters.includes(possibleTeams[i][7])) {
-                    console.log('SELECTED: ', possibleTeams[i]);
-                    const summoner = card.makeCardId(possibleTeams[i][0].toString());
-                    return [summoner, possibleTeams[i]]
-                }
-                console.log('DISCARDED: ', possibleTeams[i])
-                i++;
-            }
-            throw new Error('no team available to be played');
-        }) //select 
-        .then(async ([summoner, team]) => {
-            await page.waitForSelector(summoner);
-            await page.click(summoner)
-            try {
-                console.log(card.color(team[0]))
-                if (card.color(team[0]) === 'Gold') {
-                    console.log('TEAMCOLOR', helper.teamSplinterToPlay(team))
-                    await page.waitForXPath(`//div[@data-original-title="${helper.teamSplinterToPlay(team)}"]`, 5000).then(selector => selector.click())
-                }
-            } catch (e) {
-                console.log('ERROR AFTER Summoner: ', e)
-            }
-            await page.waitForSelector(card.makeCardId(team[1].toString()));
-            await page.click(card.makeCardId(team[1].toString()));
-            await page.waitFor(1000);
-            await team[2] ? page.waitForSelector(card.makeCardId(team[2].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 2');
-            await page.waitFor(1000);
-            await team[3] ? page.waitForSelector(card.makeCardId(team[3].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 3');
-            await page.waitFor(1000);
-            await team[4] ? page.waitForSelector(card.makeCardId(team[4].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 4');
-            await page.waitFor(1000);
-            await team[5] ? page.waitForSelector(card.makeCardId(team[5].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 5');
-            await page.waitFor(1000);
-            await team[6] ? page.waitForSelector(card.makeCardId(team[6].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 6');
-            await page.waitFor(3000);
-            await page.click('.btn-green')[0]; //start fight
-            await page.waitFor(5000);
-            console.log('DONE')
-            browser.close();
-        })
-        .catch((e) => { console.log('Error! ', e); browser.close() })
+            // }
+
+            // //TO UNCOMMENT FOR QUEST after choose the color
+            // if (matchDetails.splinters.includes('fire') && possibleTeams.find(x => x[7] === 'fire')) {
+            //     const fireTeam = possibleTeams.find(x => x[7] === 'fire')
+            //     try {
+            //         if (matchDetails.splinters.includes(helper.teamSplinterToPlay(fireTeam).toLowerCase())) {
+            //             console.log('PLAY fire: ', helper.teamSplinterToPlay(fireTeam), fireTeam)
+            //             const summoner = card.makeCardId(fireTeam[0].toString());
+            //             return [summoner, fireTeam];
+            //         }
+            //         //console.log('fire but deck not active')
+
+            //     } catch (e) {
+            //         console.log('fire DECK ERROR: ', e)
+            //     }
+            // }
+
+    page.click('.btn--create-team')[0];
+
+    try {
+        await page.waitForSelector(teamToPlay.summoner).then(summonerButton => summonerButton.click());
+        if (card.color(team[0]) === 'Gold') {
+            console.log('Dragon play TEAMCOLOR', helper.teamActualSplinterToPlay(team))
+            await page.waitForXPath(`//div[@data-original-title="${helper.teamSplinterToPlay(team)}"]`, 5000).then(selector => selector.click())
+        }
+
+        await page.waitForSelector(card.makeCardId(team[1].toString()));
+        await page.click(card.makeCardId(team[1].toString()));
+        await page.waitFor(1000);
+        await team[2] ? page.waitForSelector(card.makeCardId(team[2].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 2');
+        await page.waitFor(1000);
+        await team[3] ? page.waitForSelector(card.makeCardId(team[3].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 3');
+        await page.waitFor(1000);
+        await team[4] ? page.waitForSelector(card.makeCardId(team[4].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 4');
+        await page.waitFor(1000);
+        await team[5] ? page.waitForSelector(card.makeCardId(team[5].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 5');
+        await page.waitFor(1000);
+        await team[6] ? page.waitForSelector(card.makeCardId(team[6].toString()), { timeout: 3000 }).then(selector => selector.click()) : console.log('nocard 6');
+        await page.waitFor(3000);
+        await page.click('.btn-green')[0]; //start fight
+        await page.waitFor(5000);
+    } catch (e) {
+        console.log('Error in cards selection!');
+        await browser.close();
+        throw new Error('Error in cards selection!');
+    }
+
     await browser.close()
 }
 
@@ -149,4 +139,4 @@ async function startBotPlayMatch() {
 //     }
 // });
 
-startBotPlayMatch();
+startBotPlayMatch().catch((e) => console.log('Error: ',e));
