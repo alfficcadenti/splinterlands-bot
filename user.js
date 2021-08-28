ea4b9ccf922fce5bd2eed81e9abebaebe598eda6
Yurii Vlasenko <yurii@lotusflare.com>
2021-08-27 20:33:15 -0400

    Filter out delegated cards

--- user.js
+++ user.js
@@ -4,7 +4,7 @@ const basicCards = require('./data/basicCards'); //phantom cards available for t
 getPlayerCards = (username) => (fetch(`https://game-api.splinterlands.io/cards/collection/${username}`,
   { "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
   .then(x => x && x.json())
-  .then(x => x['cards'] ? x['cards'].map(card => card.card_detail_id) : '')
+  .then(x => x['cards'] ? x['cards'].filter(card => [null, process.env.ACCOUNT].includes(card.delegated_to)).map(card => card.card_detail_id) : '')
   .then(advanced => basicCards.concat(advanced))
   .catch(e => {console.log('Using only basic cards due to error when getting user collection from splinterlands: ',e); return basicCards})
 )
