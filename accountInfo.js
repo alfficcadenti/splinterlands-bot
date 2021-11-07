08bbfe6c0581d53a3cbb5b65199d4e7900f15850
Hector-bit <hectormartinez.2978@gmail.com>
2021-11-07 03:44:28 -0800

    accountInfo is now working

--- accountInfo.js
+++ accountInfo.js
@@ -48,3 +48,86 @@ async function getStats(page) {
     
     await page.goto('https://splinterlands.io/?p=battle_history');
     await page.waitForTimeout(8000);
+    await closePopups(page);
+
+        try {
+            console.log('Getting moneys dec');
+            const dec_selector = '#bs-example-navbar-collapse-1 > ul.nav.navbar-nav.navbar-right > li:nth-child(2) > div.dec-container > div.balance';
+            const power_selector = '#power_progress > div.progress__info > span.number_text';
+            const rank_selector = '#current_league_text';
+            const power = await page.$eval(power_selector, (element) => element.textContent);
+            const rank = await page.$eval(rank_selector, (element) => element.textContent);
+            const amount = await page.$eval(dec_selector, (element) => element.textContent);
+            const energy = await getElementTextByXpath(page, "//div[@class='dec-options'][1]/div[@class='value'][2]/div", 100);
+            if(amount){
+                    console.log(chalk.bold.red('------------------'));
+                    console.log(chalk.bold.whiteBright.cyan(`ACCOUNT: ${process.env.ACCOUNT}`));
+                    console.log('DEC AMOUNT: ', amount);
+                    console.log(chalk.bold.whiteBright.green('Your current Energy Capture Rate is ' + energy.split('.')[0] + "%"));
+                    console.log(chalk.bold.whiteBright.green(`RANK:${rank}`));
+                    console.log(chalk.bold.whiteBright.green(`POWER: ${power}`));
+                    console.log(chalk.bold.red('------------------'));
+                } else {
+                    console.log(chalk.bold.whiteBright.green('could not find the text div with dec amount'));
+                }
+            }
+        catch (e) {
+            console.info('no season reward to be claimed')
+        }
+    // }
+
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+const sleepingTimeInMinutes = process.env.MINUTES_BATTLES_INTERVAL || 30;
+const sleepingTime = sleepingTimeInMinutes * 60000;
+const isHeadlessMode = process.env.HEADLESS === 'false' ? false : true; 
+
+
+const blockedResources = [
+    'splinterlands.com/players/item_details',
+    'splinterlands.com/players/event',
+    'splinterlands.com/market/for_sale_grouped',
+    'splinterlands.com/battle/history2',
+    'splinterlands.com/players/messages',
+    'facebook.com',
+    'google-analytics.com',
+    'twitter.com',
+];
+
+async function letsgo () {
+    console.log('START ', process.env.ACCOUNT, new Date().toLocaleString())
+    const browser = await puppeteer.launch({
+        headless: isHeadlessMode, // default is true
+        args: ['--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--mute-audio',
+        '--disable-web-security']
+    }); 
+    let [page] = await browser.pages();
+
+    await page.setDefaultNavigationTimeout(500000);
+    await page.on('dialog', async dialog => {
+        await dialog.accept();
+    });
+    page.goto('https://splinterlands.io/');
+    page.recoverStatus = 0;
+    // page.favouriteDeck = process.env.FAVOURITE_DECK || '';
+    try {
+        await getStats(page)
+            .then(() => {
+                console.log('Closing session', new Date().toLocaleString());        
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+        await page.waitForTimeout(5000);        
+    } catch (e) {
+        console.log('Routine error at: ', new Date().toLocaleString(), e)
+    }
+    // await console.log(process.env.ACCOUNT,'waiting for the next battle in', sleepingTime / 1000 / 60 , ' minutes at ', new Date(Date.now() +sleepingTime).toLocaleString() )
+    // await new Promise(r => setTimeout(r, sleepingTime));
+    console.log('Process end.')
+    await browser.close();
+}
